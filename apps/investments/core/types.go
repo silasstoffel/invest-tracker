8b0bd9c4ae6ac04c6a27f4474aa6fde22e161c06
Silas Stoffel <silasstofel@gmail.com>
2025-06-16 23:00:25 -0300

    feat: adding mongo

--- apps/investments/core/types.go
+++ apps/investments/core/types.go
@@ -1,6 +1,10 @@
 package investment_core
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 const (
 	// investiment types
@@ -24,21 +28,22 @@ type BondDetail struct {
 	Rate  float64 `json:"rate"`
 }
 type InvestmentEntity struct {
-	ID             string     `json:"id"`
-	Type           string     `json:"type"`
-	Symbol         string     `json:"symbol"`
-	BondDetail     BondDetail `json:"bondDetail,omitempty"`
-	Quantity       int        `json:"quantity"`
-	UnitPrice      float64    `json:"unitPrice"`
-	TotalValue     float64    `json:"totalValue"`
-	Cost           float64    `json:"cost"`
-	OperationType  string     `json:"operationType"`
-	OperationDate  time.Time  `json:"operationDate"`
-	OperationYear  int        `json:"operationYear"`
-	OperationMonth int        `json:"operationMonth"`
-	DueDate        time.Time  `json:"dueDate"`
-	CreatedAt      time.Time  `json:"createdAt"`
-	UpdatedAt      time.Time  `json:"updatedAt"`
+	MongoID        primitive.ObjectID `bson:"_id"`
+	ID             string             `json:"id" bson:"id"`
+	Type           string             `json:"type"`
+	Symbol         string             `json:"symbol"`
+	BondIndex      BondDetail         `json:"bondDetail,omitempty"`
+	Quantity       int                `json:"quantity"`
+	UnitPrice      float64            `json:"unitPrice"`
+	TotalValue     float64            `json:"totalValue"`
+	Cost           float64            `json:"cost"`
+	OperationType  string             `json:"operationType"`
+	OperationDate  time.Time          `json:"operationDate"`
+	OperationYear  int                `json:"operationYear"`
+	OperationMonth int                `json:"operationMonth"`
+	DueDate        time.Time          `json:"dueDate"`
+	CreatedAt      time.Time          `json:"createdAt"`
+	UpdatedAt      time.Time          `json:"updatedAt"`
 }
 
 type CreateInvestmentInput struct {
